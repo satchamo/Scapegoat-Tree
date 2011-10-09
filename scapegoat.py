3f3ced7adc586723480ac53a796bfbc818c7fb64
= <matt@satchamo.com>
2011-10-09 13:29:08 -0700

    Working scapegoat

--- scapegoat.py
+++ scapegoat.py
@@ -27,9 +27,6 @@ class ScapeGoatTree():
     def isDeep(self, depth):
         return depth > self.haT()
 
-    def brother(self, x):
-        pass
-
     def brotherOf(self, parent, child):
         if parent.left != None and parent.left.key == child.key:
             return parent.right
@@ -46,18 +43,74 @@ class ScapeGoatTree():
 
     def myRebuildTree(self, x, size):
         nodes = []
-        self.myFlatten(x, nodes)
-        def helper(nodes, start, end):
+        def flatten(node, nodes):
+            if node == None:
+                return
+            flatten(node.left, nodes)
+            nodes.append(node)
+            flatten(node.right, nodes)
+
+        flatten(x, nodes)
+        #self.myFlatten(x, nodes)
+        def buildTreeFromSortedList(nodes, start, end):
             if start > end:
                 return None
-            mid = start + (end - start) / 2;
+            mid = int(math.ceil(start + (end - start) / 2.0))
             node = Node(nodes[mid].key)
-            node.left = helper(nodes, start, mid-1)
-            node.right = helper(nodes, mid+1, end)
+            node.left = buildTreeFromSortedList(nodes, start, mid-1)
+            node.right = buildTreeFromSortedList(nodes, mid+1, end)
             return node
 
-        return helper(nodes, 0, size-1)
-        
+        return buildTreeFromSortedList(nodes, 0, size-1)
+
+    def minimum(self, x):
+        while x.left != None:
+            x = x.left
+        return x
+
+    def delete(self, delete_me):
+        node = self.root
+        parent = None
+        is_left_child = True
+        while node.key != delete_me:
+            parent = node
+            if delete_me > node.key:
+                node = node.right
+                is_left_child = False
+            else:
+                node = node.left
+                is_left_child = True
+
+        # case 1: Node to be delete has no children
+        if node.left == None and node.right == None:
+            if is_left_child:
+                parent.left = None
+            else:
+                parent.right = None
+            yo
+        # case 2: Node has only a right child
+        elif node.left == None:
+            if is_left_child:
+                parent.left = node.right
+            else:
+                parent.right = node.right
+        # case 3: Node has only a left child
+        elif node.right == None:
+            if is_left_child:
+                parent.left = node.left
+            else:
+                parent.right = node.left
+        # case 4: Node has right and left child
+        else:
+            # find successor
+            successor = self.minimum(node.right)
+            # the successor is guarenteed to have no left child
+            successor.left = node.left
+            tmp = successor.right
+            successor.right = node.right
+            node.right.left = tmp
+
+
 
 
     def insert(self, key):
@@ -104,13 +157,11 @@ class ScapeGoatTree():
             
             print "found scapegoat, it's = %d, with size %d" % (scapegoat.key, sizes[I])
             #tmp = self.rebuildTree(sizes[i], scapegoat)
-            #tmp = self.rebuildTree(sizes[i]+1, scapegoat)
+            #tmp = self.rebuildTree(sizes[I]+1, scapegoat)
             tmp = self.myRebuildTree(scapegoat, sizes[I]+1)
             print "New list is:"
             self.preOrder(tmp)
             print "/"
-           
-
             
             scapegoat.left = tmp.left
             scapegoat.right = tmp.right
@@ -124,31 +175,44 @@ class ScapeGoatTree():
         b = self.sizeOf(x.right) <= (self.a * size_of_x)
         return a and b
 
-    def flatten(self, x, y):
-        if x == None:
-            return y
-        x.right = self.flatten(x.right, y)
-        return self.flatten(x.left, x)
+    def flatten(self, root, head):
+        if root == None:
+            return head
+        root.right = self.flatten(root.right, head)
+        return self.flatten(root.left, root)
 
+    #def buildTree(self, size, head):
+    #    if size == 1:
+    #        return head
+    #    elif size == 2:
+    #        head.right.left = head
+    #        return head.right
+    #    root = (self.buildTree(math.floor((size-1)/2.0), head)).right
+    #    last = self.buildTree(math.floor((size-1)/2.0), root.right)
+    #    root.left = head
+    #    return last
     def buildTree(self, n, x):
         if n == 0:
             x.left = None
             return x
-        r = self.buildTree(math.ceil((n-1)/2.0), x)
+        r = self.buildTree(math.ceil((n-1)/2.0),x)
         s = self.buildTree(math.floor((n-1)/2.0), r.right)
         r.right = s.left
         s.left = r
         return s
 
+    #def rebuildTree(self, size, scapegoat, parents):
+    #    head = self.flatten(scapegoat, None)
+    #    self.buildTree(size, head)
+    #    print "HERE"
+    #    while head.parent != None:
+    #        head = head.parent
+    #    head = parents[-1]
+    #    return head
+
     def rebuildTree(self, n, scapegoat):
-        w = Node(-20)
+        w = Node(-1)
         z = self.flatten(scapegoat, w)
-        next_ = z
-        result = []
-        while next_ != None:
-            result.append(next_.key)
-            next_ = next_.right
-        print ("flattend tree with %d nodes => " + str(result)) % (n)
         self.buildTree(n, z)
         return w.left
 
