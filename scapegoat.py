6bc2dfd34a8ec6d693a371734aa631b13de13039
= <matt@satchamo.com>
2011-10-13 09:54:24 -0700

    Better working scapegoat

--- scapegoat.py
+++ scapegoat.py
@@ -1,3 +1,4 @@
+import re
 import math
 class Node():
     def __init__(self, key):
@@ -16,6 +17,7 @@ class ScapeGoatTree():
         self.max_size = 0
         self.root = None
 
+    # Return the number of keys on the subtree rooted by x (including x's key)
     def sizeOf(self, x):
         if x == None:
             return 0
@@ -24,25 +26,19 @@ class ScapeGoatTree():
     def haT(self):
         return math.floor(math.log(self.size, 1.0/self.a))
 
+    # Determine if a specific depth of a node makes the tree "deep"
     def isDeep(self, depth):
         return depth > self.haT()
 
-    def brotherOf(self, parent, child):
-        if parent.left != None and parent.left.key == child.key:
+    # Returns the brother node of "node", whose parent is "parent"
+    def brotherOf(self, node, parent):
+        if parent.left != None and parent.left.key == node.key:
             return parent.right
-        elif parent.right != None:
-            return parent.left
-        return None
+        return parent.left
 
-    def myFlatten(self, node, nodes):
-        if node == None:
-            return
-        self.myFlatten(node.left, nodes)
-        nodes.append(node)
-        self.myFlatten(node.right, nodes)
-
-    def myRebuildTree(self, x, size):
-        nodes = []
+    # Builds a new binary tree based on an old one. The new tree is balanced
+    def myRebuildTree(self, root, length):
+        # Turn a binary tree into a list of nodes in sorted order
         def flatten(node, nodes):
             if node == None:
                 return
@@ -50,8 +46,7 @@ class ScapeGoatTree():
             nodes.append(node)
             flatten(node.right, nodes)
 
-        flatten(x, nodes)
-        #self.myFlatten(x, nodes)
+        # Build a balanced binary tree for a sort list of nodes
         def buildTreeFromSortedList(nodes, start, end):
             if start > end:
                 return None
@@ -61,17 +56,22 @@ class ScapeGoatTree():
             node.right = buildTreeFromSortedList(nodes, mid+1, end)
             return node
 
-        return buildTreeFromSortedList(nodes, 0, size-1)
+        nodes = []
+        flatten(root, nodes)
+        return buildTreeFromSortedList(nodes, 0, length-1)
 
+    # Returns the node with the minimum key in the subtree rooted by x
     def minimum(self, x):
         while x.left != None:
             x = x.left
         return x
 
+    # Delete the node in the tree with a value of delete_me
     def delete(self, delete_me):
         node = self.root
         parent = None
         is_left_child = True
+        # find the node, keep track of the parent, and side of the tree
         while node.key != delete_me:
             parent = node
             if delete_me > node.key:
@@ -81,37 +81,37 @@ class ScapeGoatTree():
                 node = node.left
                 is_left_child = True
 
+        successor = None
         # case 1: Node to be delete has no children
         if node.left == None and node.right == None:
-            if is_left_child:
-                parent.left = None
-            else:
-                parent.right = None
-            yo
+            pass
         # case 2: Node has only a right child
         elif node.left == None:
-            if is_left_child:
-                parent.left = node.right
-            else:
-                parent.right = node.right
+            successor = node.right
         # case 3: Node has only a left child
         elif node.right == None:
-            if is_left_child:
-                parent.left = node.left
-            else:
-                parent.right = node.left
+            successor = node.left
         # case 4: Node has right and left child
         else:
             # find successor
             successor = self.minimum(node.right)
-            # the successor is guarenteed to have no left child
-            successor.left = node.left
-            tmp = successor.right
-            successor.right = node.right
-            node.right.left = tmp
+            # the successor is the node's right child -- easy fix
+            if successor == node.right:
+                successor.left = node.left
+            # complicated case
+            else:
+                successor.left = node.left
+                tmp = successor.right
+                successor.right = node.right
+                node.right.left = tmp
 
-
-
+        # Replace the node
+        if parent == None:
+            self.root = None
+        elif is_left_child:
+            parent.left = successor
+        else:
+            parent.right = successor
 
     def insert(self, key):
         z = Node(key)
@@ -149,7 +149,7 @@ class ScapeGoatTree():
             sizes = [0]*len(parents)
             I = 0
             for i in range(1, len(parents)):
-                sizes[i] = sizes[i-1] + self.sizeOf(self.brotherOf(parents[i], parents[i-1])) + 1
+                sizes[i] = sizes[i-1] + self.sizeOf(self.brotherOf(parents[i-1], parents[i])) + 1
                 if not self.isAWeightBalanced(parents[i], sizes[i]+1):
                     scapegoat = parents[i]
                     I = i
@@ -226,11 +226,28 @@ class ScapeGoatTree():
         self.preOrder(self.root)
 
 
-t = ScapeGoatTree(0.57)
-#nums = [2,1,6,5,4,3,15,12,9,7,11,10,13,14, 15,16,17,18,8]
-#nums = [5,3,7,6,8,9]
-nums = [8,1,13,10,20,19,22, 29]
-for e in nums:
-    t.insert(e)
-print "----------------"
-t.printTree()
+def main():
+    f = open('tree.txt', 'r')
+    t = None
+    for line in f.readlines():
+        line = re.split(r'\s+', line) 
+        cmd = line[0]
+        if cmd == "BuildTree":
+            t = ScapeGoatTree(float(line[1]))
+            t.insert(int(line[2]))
+        elif cmd == "Insert":
+            t.insert(int(line[1]))
+
+
+
+
+main()
+#t = ScapeGoatTree(0.57)
+##nums = [2,1,6,5,4,3,15,12,9,7,11,10,13,14, 15,16,17,18,8]
+##nums = [5,3,7,6,8,9]
+#nums = [8,1,13,10,20,19,22, 29]
+#for e in nums:
+#    t.insert(e)
+##print "----------------"
+##t.delete(13)
+#t.printTree()
